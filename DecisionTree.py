4328876db34c9030572775f892a80bbfcd9c5dc9
wangzbo <42736964+wangzbo@users.noreply.github.com>
2019-08-22 00:59:44 +0800

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -12,12 +12,24 @@ class DecisionTreeNode:
 class DecisionTree:
 
     #CART algorithm
-    def __init__(self, X, y):
+    def __init__(self, X, y, max_depth = 0, classification = True):
         self.X = X
         self.y = y
-        self.root = self.buildDecisionTree(np.array(X),np.array(y))
-    
-    def _terminated(self, data, label):
+        self.max_depth = max_depth
+        if classification:
+            self.root = self.buildDT_classification(np.array(X),np.array(y),1)
+        else:
+            self.root = self.buildDT_regression(np.array(X),np.array(y),1)
+
+    def _preOrder(self, node):
+        if node != None:
+            print(node.value)
+            self._preOrder(node.left)
+            self._preOrder(node.right)
+
+    def _terminated(self, data, label, depth):
+        if depth == self.max_depth:
+            return True
         N, d = data.shape
         data_flag = [(data[0] == d).all() for d in data] == [True]*N
         label_flag = [label[0] == l for l in label] == [True]*N
@@ -34,9 +46,9 @@ class DecisionTree:
         #square error
         return np.sum([(l-np.mean(label))**2 for l in label])/(1.0*N)
 
-    def _branch_criteria(self, data, label):
+    def _branch_criteria(self, data, label, classification=True):
         N, d = data.shape
-        w_impurity, feature_index, threshold, data1, label1, data2, label2 = N,0,0.0,None,None,None,None
+        w_impurity, feature_index, threshold, data1, label1, data2, label2 = float('inf'),0,0.0,None,None,None,None
 
         for i in range(d):
             feature = data[:,i]
@@ -53,9 +65,14 @@ class DecisionTree:
                 data_part2 = np.array([data[sorted_label_index[k]] for k in range(j+1,N)])
                 label_part2 = np.array([label[sorted_label_index[k]] for k in range(j+1,N)])
 
-                weighted_impurity = self._impurity_classification(data_part1,label_part1) * data_part1.shape[0] 
-                + self._impurity_classification(data_part2,label_part2) * data_part2.shape[0]
-                                 
+                weighted_impurity = 0
+                if classification:
+                    weighted_impurity = self._impurity_classification(data_part1,label_part1) * data_part1.shape[0] 
+                    + self._impurity_classification(data_part2,label_part2) * data_part2.shape[0]
+                else:
+                    weighted_impurity = self._impurity_regression(data_part1,label_part1) * data_part1.shape[0] 
+                    + self._impurity_regression(data_part2,label_part2) * data_part2.shape[0]                
+                
                 if weighted_impurity < w_impurity:
                     w_impurity, feature_index, threshold, data1, label1, data2, label2 = weighted_impurity,i,theta,data_part1,label_part1,data_part2,label_part2
         
@@ -84,14 +101,21 @@ class DecisionTree:
     def constValue_regression(self, label):
         return np.mean(label)
 
-    def buildDecisionTree(self, data, label):
-        if self._terminated(data, label):
+    def buildDT_classification(self, data, label, depth):
+        if self._terminated(data, label, depth):
             return DecisionTreeNode(self.constValue_classification(label))
         feature_index, theta, data1, label1, data2, label2 = self._branch_criteria(data,label)
         #print((feature_index, theta))
-        return DecisionTreeNode(0,feature_index,theta,self.buildDecisionTree(data1,label1),self.buildDecisionTree(data2,label2))
+        return DecisionTreeNode(0,feature_index,theta,self.buildDT_classification(data1,label1,depth+1),self.buildDT_classification(data2,label2,depth+1))
 
-    def DT_classification(self, x):
+    def buildDT_regression(self, data, label, depth):
+        if self._terminated(data, label, depth):
+            return DecisionTreeNode(self.constValue_regression(label))
+        feature_index, theta, data1, label1, data2, label2 = self._branch_criteria(data,label,False)
+        #print((feature_index, theta))
+        return DecisionTreeNode(0,feature_index,theta,self.buildDT_regression(data1,label1,depth+1),self.buildDT_regression(data2,label2,depth+1))
+
+    def predict(self, x):
         return self.DT_predict(self.root, x)
 
     def DT_predict(self, node, x):
@@ -106,7 +130,7 @@ if __name__ == '__main__':
     X = [[1,4],[1.5,3.5],[0.5,2.5],[0.5,1],[1.5,2],[2.5,3],[2.5,1.5],[2.5,0.5],[3.5,5],[3.5,3],[3.5,1.5],[4.5,2.5],[4.5,1.5],[5,3],[5,1]]
     y = [1,1,-1,-1,-1,1,1,-1,1,-1,-1,1,-1,1,-1]
     x = [2.2,0.8]
-    dt = DecisionTree(X, y)
-    print(dt.DT_classification(x))
+    dt = DecisionTree(X, y, 7)
+    #dt._preOrder(dt.root)
+    print(dt.predict(x))
     
-
