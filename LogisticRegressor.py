d100c36e8025c60fd91b32774824531890eaa287
wangzbo <42736964+wangzbo@users.noreply.github.com>
2019-09-23 11:34:28 +0800

    Update LogisticRegressor.py

--- LogisticRegressor.py
+++ LogisticRegressor.py
@@ -2,14 +2,19 @@ import numpy as np
 
 class LogisticRegressor:
 
+    '''
+    Attributes:
+        X: input features of train data
+        y: labels of train data
+    '''
     def __init__(self, X, y):
         self.X = X
         self.y = y
 
-    def __sigmoid(self, s):
+    def _sigmoid(self, s):
         return 1.0/(1+np.exp(-s))
 
-    def logisticRegression(self, iteration = 100):
+    def logisticRegression(self, iteration = 100, learning_rate = 0.001):
         train_data, train_label = np.array(self.X), np.array(self.y)
         N, d = train_data.shape
         train_data = np.hstack((np.ones((N,1)), train_data))
@@ -19,15 +24,14 @@ class LogisticRegressor:
         for i in range(iteration):
             gradient = 0
             for j in range(N):
-                gradient = gradient + self.__sigmoid(-1*train_label[j]*np.inner(w,train_data[j]))*(-1)*train_label[j]*train_data[j]
+                gradient = gradient + self._sigmoid(-1*train_label[j]*np.inner(w,train_data[j]))*(-1)*train_label[j]*train_data[j]
                 
             gradient /= (N*1.0)
-            learn_rate = 0.01
-            w = w - learn_rate*gradient
+            w = w - learning_rate*gradient
 
         return w/np.sqrt(np.sum(np.square(w)))
 
-    def logisticRegressionReg(self, iteration = 100, C = 1.0):
+    def logisticRegressionReg(self, iteration = 100, learning_rate = 0.001, C = 1.0):
         train_data, train_label = np.array(self.X), np.array(self.y)
         N, d = train_data.shape
         train_data = np.hstack((np.ones((N,1)), train_data))
@@ -36,15 +40,12 @@ class LogisticRegressor:
         #SGD
         for i in range(iteration):
             j = np.random.randint(0, N)
-            gradient = self.__sigmoid(-1*train_label[j]*np.inner(w,train_data[j]))*(-1)*train_label[j]*train_data[j]+1.0/(N*C)*w
-            learn_rate = 0.01
-            w = w - learn_rate*gradient
+            gradient = self._sigmoid(-1*train_label[j]*np.inner(w,train_data[j]))*(-1)*train_label[j]*train_data[j]+1.0/(N*C)*w            
+            w = w - learning_rate*gradient
 
         return w/np.sqrt(np.sum(np.square(w)))
 
-        
-
 if __name__ == '__main__':
     l = LogisticRegressor([[1,1],[0,0],[1,-1],[0,1.5],[-2,1],[-1,5],[2,2.9],[-1,0.1],[3,4.1],[-4,-3.1]],[1,1,1,-1,-1,-1,1,-1,-1,1])
-    print(l.logisticRegressionReg(100000,10))
+    print(l.logisticRegressionReg(100000))
 
